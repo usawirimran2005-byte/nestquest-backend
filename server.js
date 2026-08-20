74cc86fc8c07486c026aa244ed06a1dfe496a56c
Usawir Imran <usawirimran2005@gmail.com>
2026-08-19 21:12:45 -0700

    Force CORS headers at vercel and express level

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
-app.use(cors());
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
