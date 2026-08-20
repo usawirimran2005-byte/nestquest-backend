31c1cafab6b784a97badb424d188c1e67cc9f492
Usawir Imran <usawirimran2005@gmail.com>
2026-08-19 21:49:48 -0700

    Fix unhandled MongoDB connection crash

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
-    throw error;
   }
 };
 
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+connectDB().catch((err) => console.error("DB connection error:", err.message));
 
 app.get("/", (req, res) => {
   res.send("NestQuest Backend is Running");
